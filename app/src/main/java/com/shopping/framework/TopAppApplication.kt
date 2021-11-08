c476f4af413164b49026beabb36fcbe6d41c9fbe
Mehdi Hosseinzade <arshen@outlook.com>
2021-11-08 11:22:42 +0330

    set default theme nightmode

--- app/src/main/java/com/shopping/framework/TopAppApplication.kt
+++ app/src/main/java/com/shopping/framework/TopAppApplication.kt
@@ -1,6 +1,7 @@
 package com.shopping.framework
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.shopping.framework.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -11,6 +12,7 @@ import org.koin.core.context.startKoin
 class TopAppApplication : Application() {
     @KoinExperimentalAPI
     override fun onCreate() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         super.onCreate()
         startKoin {
             androidLogger()
