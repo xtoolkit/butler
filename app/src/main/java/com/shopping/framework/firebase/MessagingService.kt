6f7750f945e88b3d201b1716c91a9fc115691bbe
Mehdi Hosseinzade <arshen@outlook.com>
2021-12-11 12:08:24 +0330

    init firebase

---
+++ app/src/main/java/com/shopping/framework/firebase/MessagingService.kt
@@ -0,0 +1,9 @@
+package com.shopping.framework.firebase
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MessagingService: FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+
+    }
+}
