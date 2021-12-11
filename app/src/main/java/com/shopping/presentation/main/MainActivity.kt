de780bc63f05996522a83a24d0ece3ea3c302f27
Mehdi Hosseinzade <arshen@outlook.com>
2021-12-11 12:38:41 +0330

    fix

--- app/src/main/java/com/shopping/presentation/main/MainActivity.kt
+++ app/src/main/java/com/shopping/presentation/main/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.shopping.presentation.main
 
+import android.content.res.Configuration
 import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.content.edit
 import androidx.core.os.bundleOf
 import androidx.lifecycle.lifecycleScope
 import com.discord.panels.OverlappingPanelsLayout
@@ -19,7 +20,10 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainActivity : AppCompatActivity(), PanelsChildGestureRegionObserver.GestureRegionsListener {
     private var binding: ActivityMainBinding? = null
     private val viewModel: MainActivityViewModel by viewModel()
-    private val setting by lazy { getSharedPreferences("mainActivitySetting", MODE_PRIVATE) }
+    private val shoplistId by lazy { intent.getIntExtra("shoplistId", -1) }
+    private val isRTL by lazy { resources.configuration.layoutDirection == View.LAYOUT_DIRECTION_RTL }
+    private val isLandscape by lazy { resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE }
+    private var isNavigationInRight = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,14 +35,9 @@ class MainActivity : AppCompatActivity(), PanelsChildGestureRegionObserver.Gestu
     private fun initView() = binding?.apply {
         viewModel.on(CLOSE_PANELS) { overlappingPanels.closePanels() }
         viewModel.on(SHOW_SHOP_LIST_ITEMS) { it: Int ->
-            supportFragmentManager.apply {
-                setFragmentResult("shopListChanged", bundleOf("id" to it))
-                setFragmentResult("shopListItemsChanged", bundleOf("id" to it))
-                setFragmentResult("shopListManageChanged", bundleOf("id" to it))
-            }
+            supportFragmentManager.setFragmentResult("shopListChanged", bundleOf("id" to it))
         }
-
-        viewModel.start(setting.getInt("lastListSelected", -1))
+        viewModel.start(shoplistId)
 
         overlappingPanels.apply {
             registerStartPanelStateListeners(object : OverlappingPanelsLayout.PanelStateListener {
@@ -62,11 +61,24 @@ class MainActivity : AppCompatActivity(), PanelsChildGestureRegionObserver.Gestu
             }
         }
 
+        var statusBarHeight: Int? = null
+        val outRect = Rect()
+        (window.decorView as ViewGroup).getWindowVisibleDisplayFrame(outRect)
+        isNavigationInRight = resources.displayMetrics.widthPixels == outRect.bottom
+
+        resources.getIdentifier("status_bar_height", "dimen", "android").takeIf { it > 0 }?.let {
+            statusBarHeight = resources.getDimensionPixelSize(it)
+            if (isLandscape) setNotches(statusBarHeight!!)
+        }
+
+        if (!isLandscape) resources.getIdentifier("navigation_bar_height", "dimen", "android")
+            .takeIf { it > 0 }?.let {
+                setNotches(statusBarHeight!!, resources.getDimensionPixelSize(it))
+            }
+
         supportFragmentManager.apply {
             setFragmentResultListener("onSelectedShopListChange", this@MainActivity) { _, it ->
-                val id = it.getInt("id")
-                viewModel.changeShowShopList(id)
-                setting.edit { putInt("lastListSelected", id) }
+                viewModel.changeShowShopList(it.getInt("id"))
             }
 
             setFragmentResultListener("openShopListMenu", this@MainActivity) { _, _ ->
@@ -81,11 +93,13 @@ class MainActivity : AppCompatActivity(), PanelsChildGestureRegionObserver.Gestu
                         AppCompatDelegate.MODE_NIGHT_YES
                 )
             }
-
-            setFragmentResultListener("requestOpenShopListSetting", this@MainActivity) { _, _ ->
-                overlappingPanels.openEndPanel()
-            }
         }
+    }
+
+    private fun setNotches(top: Int, bottom: Int = 0) = binding?.apply {
+        startPanelFrame.setPadding(0, top, 0, bottom)
+        centerPanelFrame.setPadding(0, top, 0, bottom)
+        endPanelFrame.setPadding(0, top, 0, bottom)
     }
 
     override fun onGestureRegionsUpdate(gestureRegions: List<Rect>) {
