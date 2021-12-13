f09038d8715873e085d9d9ceb91dc54f045a1ac9
Mehdi Hosseinzade <arshen@outlook.com>
2021-12-13 17:47:34 +0330

    finish first activity

--- app/src/main/java/io/github/xtoolkit/butler/presentation/main/MainActivity.kt
+++ app/src/main/java/io/github/xtoolkit/butler/presentation/main/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startActivity(Intent(this, ShoppingMainActivity::class.java))
+        finish()
     }
 }
