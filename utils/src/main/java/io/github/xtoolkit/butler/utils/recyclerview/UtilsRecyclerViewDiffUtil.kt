339fcb5a962c43093200a47d5c2517aee7bf8c6f
Mehdi Hosseinzade <arshen@outlook.com>
2022-01-02 10:53:21 +0330

    fix proguard isuses

--- utils/src/main/java/io/github/xtoolkit/butler/utils/recyclerview/UtilsRecyclerViewDiffUtil.kt
+++ utils/src/main/java/io/github/xtoolkit/butler/utils/recyclerview/UtilsRecyclerViewDiffUtil.kt
@@ -3,12 +3,11 @@ package io.github.xtoolkit.butler.utils.recyclerview
 import android.annotation.SuppressLint
 import androidx.recyclerview.widget.DiffUtil
 
+@SuppressLint("DiffUtilEquals")
 class UtilsRecyclerViewDiffUtil<Item> : DiffUtil.ItemCallback<Item>() {
-    override fun areItemsTheSame(oldItem: Item, newItem: Item) =
-        getId(oldItem as Any) == getId(newItem as Any)
+    override fun areItemsTheSame(oldItem: Item, newItem: Item) = oldItem == newItem
 
-    @SuppressLint("DiffUtilEquals")
     override fun areContentsTheSame(oldItem: Item, newItem: Item) = oldItem == newItem
 
-    private fun getId(x: Any) = x::class.java.getMethod("getId").invoke(x) as Int
+//    private fun getId(x: Any) = x::class.java.getMethod("getId").invoke(x) as Int
 }
